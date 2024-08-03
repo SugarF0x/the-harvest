db3536c21ce1d417bf3caf1324d12501b73e2402
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-08-03 03:10:54 +0300

    add camera vertical pan

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,22 +4,24 @@ extends CharacterBody3D
 const SPEED := 5.0
 const JUMP_VELOCITY := 4.5
 const SENSITIVITY := 0.3
-const CAMERA_PAN_CLAMP := 0.1
+const CAMERA_PAN_CLAMP := 10.0
 
 var camera_distance := 3.0
 var camera_elevation := 3.5
-var camera_pan := 1
+var camera_pan := 0.0
 
 var prev_mouse_position := Vector2()
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_3d = $Camera3D
 @onready var body = $Body
+@onready var initial_pan = rad_to_deg(camera_3d.rotation.x)
 
 func _input(event):
 	handle_mouse_controls(event)
-	handle_camera_movement(event)
+	handle_camera_rotation(event)
 	handle_mouse_follow(event)
+	handle_camera_zoom(event)
 
 func _physics_process(delta):
 	if not is_on_floor(): velocity.y -= gravity * delta
@@ -49,19 +51,25 @@ func handle_mouse_controls(event: InputEvent):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_viewport().warp_mouse(prev_mouse_position)
 
-func handle_camera_movement(event: InputEvent):
+func handle_camera_rotation(event: InputEvent):
 	if not event is InputEventMouseMotion: return
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT): return
 	
 	var mouse_delta = event.relative
 	rotate_y(deg_to_rad(-mouse_delta.x * SENSITIVITY))
 	
+	var new_pan = clampf(rad_to_deg(camera_3d.rotation.x) - mouse_delta.y * SENSITIVITY, initial_pan - CAMERA_PAN_CLAMP, initial_pan + CAMERA_PAN_CLAMP)
+	camera_3d.rotation.x = deg_to_rad(new_pan)
+
+func handle_camera_zoom(event: InputEvent):
+	var direction = 0
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_UP): direction = 1
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_DOWN): direction = -1
+
 func handle_mouse_follow(event: InputEvent):
 	if not event is InputEventMouseMotion: return
-	# Get the mouse position in the viewport
 	var mouse_position = event.position
 
-	# Convert the mouse position to a ray in world space
 	var from = camera_3d.project_ray_origin(mouse_position)
 	var to = camera_3d.project_ray_normal(mouse_position) * 1000
 	
